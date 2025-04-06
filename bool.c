29d73ec2866e5ebbf2696eea2806c7d2231b21c9
NureAlam68 <nurealam151068@gmail.com>
2025-04-06 20:45:33 +0600

    boolean in c

---
+++ bool.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    bool isTrue = true;
+    bool isFalse = false;
+    printf("isTrue: %d\n", isTrue);   // Output: 1 (true)
+    printf("isFalse: %d\n", isFalse); // Output: 0 (false)
+    return 0;
+}
