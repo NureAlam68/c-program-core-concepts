4d0882a8fdbd361bfd4afa534b7bb54bc050f576
NureAlam68 <nurealam151068@gmail.com>
2025-04-06 20:39:28 +0600

    variable in c

--- first.c
+++ first.c
@@ -1,7 +1,21 @@
-#include<stdio.h>
+// #include <stdio.h>
+// int main() {
+//     printf("Hello, World!!!\n");
+//     printf("This is my first C program\n");
+//     printf("I am learning C programming");
+//     return 0;
+// } 
+
+// variable declaration and initialization
+#include <stdio.h>
 int main() {
-    printf("Hello, World!!!\n");
-    printf("This is my first C program\n");
-    printf("I am learning C programming");
+    int a = 10; // integer variable
+    float b = 20.5; // float variable
+    char c = 'A'; // character variable
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+    printf("Character: %c\n", c);
+
     return 0;
 }
